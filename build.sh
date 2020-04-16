c85d9ec1b727fce6e46cc280193b3298f199c4a1
Georg Schneider <georg.schneider@gmail.com>
2020-04-16 09:12:12 +0200

    Update to SDK 1.0.0 (#40)
    * Update the SDK 1.0.0
    
    * Remove scenario initialization

--- build.sh
+++ build.sh
@@ -31,9 +31,19 @@ function cleanDars {
 function buildDars {
   for dr in ${DIRS[@]}
   do
-    echo "building dar in $dr"
+    echo "Building dar in $dr"
     cd $dr
     daml build
+    cd ..
+  done
+}
+
+function testDars {
+  for dr in ${DIRS[@]}
+  do
+    echo "Testing dar in $dr"
+    cd $dr
+    daml test
     cd ..
   done
 }
@@ -48,8 +58,10 @@ function updateVersion {
     cat $dr/daml.yaml | sed -E "s/^(sdk-version:).*/\\1 $1/" > tmp.yml
     mv tmp.yml $dr/daml.yaml
   done
-#  cat ./.circleci/config.yml | sed -E "s/daml-sdk:.*/daml-sdk:$1/" > tmp.yml
-#  mv tmp.yml ./.circleci/config.yml
+  cat daml.yaml | sed -E "s/^(sdk-version:).*/\\1 $1/" > tmp.yml
+  mv tmp.yml daml.yaml
+  cat ./.circleci/config.yml | sed -E "s/daml-sdk:.*/daml-sdk:$1/" > tmp.yml
+  mv tmp.yml ./.circleci/config.yml
 }
 
 case $1 in
@@ -60,6 +72,9 @@ case $1 in
   build-dars)
     cleanDars
     buildDars
+    ;;
+  test-dars)
+    testDars
     ;;
   *)
     echo "builder <command>"
